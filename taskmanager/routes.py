9a16a2390d855b00541bab70d57035d0bb0dbca4
adampl2 <adamchwiulkowski@gmail.com>
2023-03-18 03:21:40 +0000

    Delete task

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
 from taskmanager import app, db
 from taskmanager.models import Category, Task
 
